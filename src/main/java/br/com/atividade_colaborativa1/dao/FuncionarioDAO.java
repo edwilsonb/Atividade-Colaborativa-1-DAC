f6d3e09ee851290d4fbe7103a1d2db1db9ae485f
IagoGOOl <iago.gool@gmail.com>
2024-03-23 18:29:15 -0300

    Criando o Inserir funcionario

--- src/main/java/br/com/atividade_colaborativa1/dao/FuncionarioDAO.java
+++ src/main/java/br/com/atividade_colaborativa1/dao/FuncionarioDAO.java
@@ -1,7 +1,10 @@
 package br.com.atividade_colaborativa1.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import br.com.atividade_colaborativa1.conexao.FabricaDeConexao;
 import br.com.atividade_colaborativa1.entidades.Funcionario;
 
 public class FuncionarioDAO {
@@ -9,16 +12,27 @@ public class FuncionarioDAO {
     private Connection connection;
 
     
-    public FuncionarioDAO (){
-
+    public FuncionarioDAO ()throws ClassNotFoundException{
+        this.connection = new FabricaDeConexao().getConnection();
     }
 
-    public inserFunmcionario (Funcionario funcionario){
+    public void insereFuncionario (Funcionario funcionario){
 
-        return null;
+        String query  = "INSERT INTO funcionario ( nome_completo, matricula ) values (?, ?)";
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, funcionario.getNomeCompleto());
+            statement.setString(2, funcionario.getMatricula());
+
+            statement.execute();
+
+            statement.close();
+        } catch (SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 
-    public Funcionario byId(Long ind){
+    public Funcionario byId(Long ind){ 
 
         return null;
     }
